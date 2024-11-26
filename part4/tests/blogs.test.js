5213cdf0f2ea7cbcadb225e23db3b39c90cad3c9
Eggeater48 <cealestias@gmail.com>
2024-11-26 13:42:22 +0200

    completed 4.8

--- part4/tests/blogs.test.js
+++ part4/tests/blogs.test.js
@@ -1,4 +1,5 @@
 const { test, after , beforeEach } = require('node:test')
+const assert = require("node:assert");
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -7,72 +8,75 @@ const api = supertest(app)
 
 const Blog = require('../models/Blog')
 
-const initialBlogs = [
-	{
-		_id: "5a422a851b54a676334d17f7",
-		title: "React patterns",
-		author: "Michael Chan",
-		url: "https://reactpatterns.com/",
-		likes: 7,
-		__v: 0
-	},
-	{
-		_id: "5a422aa71b54a676234d17f8",
-		title: "Go To Statement Considered Harmful",
-		author: "Edsger W. Dijkstra",
-		url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-		likes: 5,
-		__v: 0
-	},
-	{
-		_id: "5a422b3a1b54a676234d17f9",
-		title: "Canonical string reduction",
-		author: "Edsger W. Dijkstra",
-		url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-		likes: 12,
-		__v: 0
-	},
-	{
-		_id: "5a422b891b54a676234d17fa",
-		title: "First class tests",
-		author: "Robert C. Martin",
-		url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-		likes: 10,
-		__v: 0
-	},
-	{
-		_id: "5a422ba71b54a676234d17fb",
-		title: "TDD harms architecture",
-		author: "Robert C. Martin",
-		url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-		likes: 0,
-		__v: 0
-	},
-	{
-		_id: "5a422bc61b54a676234d17fc",
-		title: "Type wars",
-		author: "Robert C. Martin",
-		url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-		likes: 2,
-		__v: 0
-	}
-]
-
 beforeEach(async () => {
+	const initialBlogs = [
+		{
+			_id: "5a422a851b54a676334d17f7",
+			title: "React patterns",
+			author: "Michael Chan",
+			url: "https://reactpatterns.com/",
+			likes: 7,
+			__v: 0
+		},
+		{
+			_id: "5a422aa71b54a676234d17f8",
+			title: "Go To Statement Considered Harmful",
+			author: "Edsger W. Dijkstra",
+			url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+			likes: 5,
+			__v: 0
+		},
+		{
+			_id: "5a422b3a1b54a676234d17f9",
+			title: "Canonical string reduction",
+			author: "Edsger W. Dijkstra",
+			url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+			likes: 12,
+			__v: 0
+		},
+		{
+			_id: "5a422b891b54a676234d17fa",
+			title: "First class tests",
+			author: "Robert C. Martin",
+			url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+			likes: 10,
+			__v: 0
+		},
+		{
+			_id: "5a422ba71b54a676234d17fb",
+			title: "TDD harms architecture",
+			author: "Robert C. Martin",
+			url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+			likes: 0,
+			__v: 0
+		},
+		{
+			_id: "5a422bc61b54a676234d17fc",
+			title: "Type wars",
+			author: "Robert C. Martin",
+			url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+			likes: 2,
+			__v: 0
+		}
+	]
 	await Blog.deleteMany({})
 
-	initialBlogs.forEach((blog) => {
-		let blogObject = new Blog(blog)
-		blogObject.save()
-	})
+	await Blog.insertMany(initialBlogs)
 })
 
 
-test('blogs returned as json and right amount of blogs', async () => {
-	await api
+test('blog api returns 200, blogs as json and the correct length', async () => {
+	const response = await api
 		.get('/api/blogs')
 		.expect(200)
 		.expect('Content-Type', /application\/json/)
+	assert.strictEqual(response.body.length, 6)
+})
+
+test('check if blog.id is actually id', async () => {
+	const response = await api
+		.get('/api/blogs')
+
 })
 
 after(async () => {
